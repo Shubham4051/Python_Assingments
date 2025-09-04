78f73a4d697af1f92328c2928a98e93cbdd31248
Shubham4051 <shubhamadarsh6017@gmail.com>
2025-09-04 13:02:26 +0530

    Assignment 3 complete

---
+++ Assingment_3/Task1.py
@@ -0,0 +1,13 @@
+# Task 1: Calculate Factorial Using a Function
+
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+
+n = int(input("Enter a number: "))
+a = factorial(n)
+print("Factorial of",n,"is:",a)
